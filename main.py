c623e3d3d27b30c2fd93504d5830cae74e082f66
Seung, SaGong <79702151+ys05086@users.noreply.github.com>
2025-11-04 23:47:09 +0900

    Update print statements and change model to ResNet152

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ path = 'E:/AdvancedDL/Project2/'
 z_train = zipfile.ZipFile(path + 'train.zip', 'r')
 z_train_list = z_train.namelist() # name list of all images in zip file
 train_cls = ftn.read_gt(path + 'train_gt.txt', len(z_train_list))
-print(train_cls)
+print('%d Train labels loaded.' % len(train_cls))
 
 mean, std = ftn.mean_std_calculation(z_train, z_train_list)
 print('Mean:', mean)
@@ -35,14 +35,14 @@ print('Std:', std)
 z_test = zipfile.ZipFile(path + 'test.zip', 'r')
 z_test_list = z_test.namelist() # name list of all images in zip file
 test_cls = ftn.read_gt(path + 'test_gt.txt', len(z_test_list))
-print(test_cls)
+print('%d Test labels loaded.' % len(test_cls))
 
 
 # train_images, train_cls, mean, std = ftn.load_image(train_path, 207005, preprocessing = True)
 # test_images, test_cls, mean_test, std_test = ftn.load_image(test_path, 10000, mean, std, preprocessing = True)
 
 # ----------- Model ------------------ #
-model = ftn.ResNet101(outputsize = 200).to(DEVICE)
+model = ftn.ResNet152(outputsize = 200).to(DEVICE)
 
 # ----------- Optimizer -------------- #
 optimizer = torch.optim.SGD(model.parameters(), weight_decay = 1e-4, momentum = 0.9, lr = learning_rate)
